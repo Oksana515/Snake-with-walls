cccb007d956cf4864643048d1d36899e32409370
Oksana Tarnavska <oxy_101@ukr.net>
2023-06-21 10:02:54 +0300

    showing score in real time

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ clock = pg.time.Clock()
 
 W = 420
 H = 420
-screen = pg.display.set_mode((W, H))
+screen = pg.display.set_mode((W, H + 100))
 pg.display.set_caption('Snake')
 
 my_font = pg.font.SysFont(None, 30)
 
 block_size = 20
 
-block_img = pg.image.load('block.png')
+block_img = pg.image.load('block_yellow.png')
 block_img = pg.transform.scale(block_img, (block_size, block_size))
 
 
@@ -23,9 +23,8 @@ def draw_text(text, font, t_color, x_text, y_text):
     screen.blit(font_img, (x_text, y_text))
 
 
-chocolate_color = "#815854"
-cream_color = "#F9EBDE"
-BG = chocolate_color
+# define colors
+colours = {'bg': "#06172d", 'snake': "#dac049", 'score': "#a2b2b8"}
 
 
 def shift_coord(some_list, some_length):
@@ -42,6 +41,18 @@ def increase_snake_n_moving_food(s_list, length, some_x, some_y):
     return s_list, length, some_x, some_y
 
 
+def moving_through_the_field_size(some_coords):
+    if some_coords[0] == W:
+        some_coords[0] = 0
+    elif some_coords[0] == -block_size:
+        some_coords[0] = W
+    elif some_coords[1] == -block_size:
+        some_coords[1] = H - block_size
+    elif some_coords[1] == H:
+        some_coords[1] = 0
+    return some_coords
+
+
 x = None
 y = None
 is_moving = None
@@ -53,7 +64,6 @@ snake_list = None
 game_paused = None
 pause_counter = None
 game_over = None
-
 
 
 def set_initial_parameters():
@@ -88,32 +98,25 @@ while run:
 
     clock.tick(10)
 
-    screen.fill(BG)
+    screen.fill(colours['bg'])
 
-    # drawing food
-    if not game_over:
-        pg.draw.rect(screen, cream_color, (food_x, food_y, block_size, block_size))
-
-    # displaying score at the end of a game
-    if game_over:
-        draw_text('Game Over', my_font, cream_color, 160, 130)
-        draw_text('Your score =', my_font, cream_color, 140, 180)
-        screen.blit(my_font.render(str(snake_length - 4), True, cream_color), (270, 180))
-        draw_text('Press R to restart', my_font, cream_color, 130, 250)
-
-    # collision with itself
-    for i in range(snake_length - 3):
-        if snake_list[0][0] == snake_list[i+3][0] and snake_list[0][1] == snake_list[i+3][1]:
-            snake_speed = 0
-            game_over = True
+    # displaying score and level number
+    pg.draw.rect(screen, colours['score'], pg.Rect(0, 420, W, 100))
+    draw_text('Level 1', my_font, colours['bg'], 40, 440)
+    draw_text('Pause: P', my_font, colours['bg'], 300, 440)
+    draw_text('Your score =', my_font, colours['bg'], 40, 480)
+    screen.blit(my_font.render(str(snake_length - 4), True, colours['bg']), (170, 480))
 
     # initial moving of the snake
     if is_moving['direction'] == 0:
         if not game_paused:
             shift_coord(snake_list, snake_length)
             snake_list[0][0] += snake_speed
+            # moving through the field sides
+            snake_list[0] = moving_through_the_field_size(snake_list[0])
         if snake_list[0][0] == food_x - block_size and snake_list[0][1] == food_y:
-            snake_list, snake_length, food_x, food_y = increase_snake_n_moving_food(snake_list, snake_length, food_x, food_y)
+            snake_list, snake_length, food_x, food_y = increase_snake_n_moving_food(snake_list, snake_length, food_x,
+                                                                                    food_y)
         for i in range(snake_length):
             if snake_list[i][0] == W:
                 snake_list[i][0] = 0
@@ -122,29 +125,42 @@ while run:
         # coordinates of a head on the next step
         next_head_pos = [snake_list[0][0], snake_list[0][1]]
         next_head_pos[is_moving['axis']] += is_moving['direction'] * snake_speed
+        # moving through the field sides
+        next_head_pos = moving_through_the_field_size(next_head_pos)
         # if head eats food on the next step, block with the food coordinates is added to the head of the snake
         # and the food block is moved on it's next position
         if next_head_pos[0] == food_x and next_head_pos[1] == food_y:
-            snake_list, snake_length, food_x, food_y = increase_snake_n_moving_food(snake_list, snake_length, food_x, food_y)
+            snake_list, snake_length, food_x, food_y = increase_snake_n_moving_food(snake_list, snake_length, food_x,
+                                                                                    food_y)
         # change_coord shifts coordinates of length - 1 elements and next row changes coordinates of the head
         if not game_paused:
             shift_coord(snake_list, snake_length)
             snake_list[0][is_moving['axis']] += is_moving['direction'] * snake_speed
 
     # moving through the field sides
-    if snake_list[0][0] == W:
-        snake_list[0][0] = 0
-    elif snake_list[0][0] == -block_size:
-        snake_list[0][0] = W
-    elif snake_list[0][1] == -block_size:
-        snake_list[0][1] = H
-    elif snake_list[0][1] == H:
-        snake_list[0][1] = 0
+    snake_list[0] = moving_through_the_field_size(snake_list[0])
 
     # drawing the snake
     if not game_over:
         for i in range(snake_length):
             screen.blit(block_img, (snake_list[i][0], snake_list[i][1]))
+
+    # drawing food
+    if not game_over:
+        pg.draw.rect(screen, colours['snake'], (food_x, food_y, block_size, block_size))
+
+    # collision with itself
+    for i in range(snake_length - 3):
+        if snake_list[0][0] == snake_list[i + 3][0] and snake_list[0][1] == snake_list[i + 3][1]:
+            snake_speed = 0
+            game_over = True
+
+    # displaying score at the end of a game
+    if game_over:
+        draw_text('Game Over', my_font, colours['score'], 160, 130)
+        draw_text('Your score =', my_font, colours['score'], 140, 180)
+        screen.blit(my_font.render(str(snake_length - 4), True, colours['score']), (270, 180))
+        draw_text('Press R to restart', my_font, colours['snake'], 130, 250)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
